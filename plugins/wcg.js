1e5a8b439ad2ebf01900b570f5d4b741926b2940
inrl-official <fasweehmon2@gmail.com>
2023-10-13 14:47:50 +0530

    Update wcg.js

--- plugins/wcg.js
+++ plugins/wcg.js
@@ -8,5 +8,6 @@ inrl({
     fromMe: 'public'
 }, async (m, match) => {
     if(m.jid == '120363040291283569@g.us') return await m.send('invalid attempt');//m.send('_*wcg*_
+    let try_to_start = new WCG(m);
     try_to_start.start();
 });

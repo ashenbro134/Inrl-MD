3d44b3221ce92361fcc18cb5f851eb58ca27b7bb
inrl-official <fasweehmon2@gmail.com>
2023-10-13 15:49:31 +0530

    Update wcg.js

--- plugins/wcg.js
+++ plugins/wcg.js
@@ -7,7 +7,7 @@ inrl({
     desc: "Word Chain game",
     fromMe: 'public'
 }, async (m, match) => {
-    if(m.jid == '120363040291283569@g.us') return await m.send('invalid attempt');//m.send('_*wcg*_
+    //if(m.jid == '120363040291283569@g.us') return await m.send('invalid attempt');//m.send('_*wcg*_
     let try_to_start = new WCG(m);
     try_to_start.start();
 });
